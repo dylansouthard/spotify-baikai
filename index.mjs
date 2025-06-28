7b6d50ef5ccde876203cab03f20182111400c057
Dylan Southard <southard.dylan@gmail.com>
2025-06-28 23:49:01 +0900

    adjust index

--- index.mjs
+++ index.mjs
@@ -10,19 +10,6 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
-process.on('uncaughtException', (err) => {
-  errorLogStream.write(`Uncaught Exception: ${err.stack}\n`)
-})
-
-process.on('unhandledRejection', (reason, promise) => {
-  errorLogStream.write(`Unhandled Rejection: ${reason}\n`)
-})
-
-console.error = function (...args) {
-  errorLogStream.write(args.map(String).join(' ') + '\n')
-  process.stderr.write(args.map(String).join(' ') + '\n')
-}
-
 app.use(cors())
 app.use(express.json())
 
